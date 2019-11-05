2f748eff1aebb524dddae2b45e722b68f28dde3f
ChenYong <chenyong@jimeng.io>
2019-11-05 17:10:18 +0800

    export Tab types; alpha release

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { default as JimoButton } from "./jimo-button";
-export { default as JimoTabs } from "./jimo-tabs";
+export { default as JimoTabs, IJimoTabItem } from "./jimo-tabs";
 export { default as UnderlineTabs } from "./underline-tabs";
 export { default as TodoFeature } from "./todo-feature";
