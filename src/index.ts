3c4a7feaaddd236831a5b6cf5a4e4095cc2005be
ChenYong <chenyong@jimeng.io>
2019-12-12 18:51:51 +0800

    release 0.0.6 to fix path for LoadingArea

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ export { default as JimoTabs, IJimoTabItem } from "./jimo-tabs";
 export { default as UnderlineTabs } from "./underline-tabs";
 export { default as TodoFeature } from "./todo-feature";
 export { default as LoadingIndicator } from "./loading-indicator";
-export { default as LoadingArea } from "./loading-indicator";
+export { default as LoadingArea } from "./loading-area";
 export { default as ClampText } from "./clamp-text";
