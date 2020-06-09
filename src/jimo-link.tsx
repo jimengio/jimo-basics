e20d6fe5c827909bd2ed705b96db2903ef45c594
ChenYong <chenyong@jimeng.io>
2020-06-09 18:44:44 +0800

    more default props on link

--- src/jimo-link.tsx
+++ src/jimo-link.tsx
@@ -1,15 +1,21 @@
-import React, { FC } from "react";
+import React, { FC, ReactNode } from "react";
 import { css, cx } from "emotion";
 
+interface IProps extends React.DetailedHTMLProps<React.AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement> {
+  text?: ReactNode;
+  className?: string;
+  onClick: () => void;
+}
+
 /** Jimo 主题的按钮, 定制了颜色样式 */
-let JimoLink: FC<{ text: string; className?: string; onClick: () => void }> = React.memo((props) => {
+let JimoLink: FC<IProps> = React.memo((props) => {
   /** Plugins */
   /** Methods */
   /** Effects */
   /** Renderers */
   return (
     <a tabIndex={0} className={cx(styleLink, props.className)} onClick={props.onClick}>
-      {props.text}
+      {props.text || props.children || "-"}
     </a>
   );
 });
@@ -26,6 +32,7 @@ let styleLink = css`
   }
 
   &:focus {
+    color: hsl(222, 100%, 72%);
     outline: none;
   }
 
