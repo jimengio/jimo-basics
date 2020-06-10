056ad08f6e3151a0bbe189c37f8cea407ec9d85f
ChenYong <chenyong@jimeng.io>
2020-06-10 17:49:42 +0800

    simplify onClick definition in JimoLink

--- src/jimo-link.tsx
+++ src/jimo-link.tsx
@@ -1,14 +1,13 @@
 import React, { FC, ReactNode } from "react";
 import { css, cx } from "emotion";
 
-interface IProps extends React.DetailedHTMLProps<React.AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement> {
+interface JimoLinkProps extends React.DetailedHTMLProps<React.AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement> {
   text?: ReactNode;
   className?: string;
-  onClick: (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => void;
 }
 
 /** Jimo 主题的按钮, 定制了颜色样式 */
-let JimoLink: FC<IProps> = React.memo((props) => {
+let JimoLink: FC<JimoLinkProps> = React.memo((props) => {
   /** Plugins */
   /** Methods */
   /** Effects */
@@ -17,7 +16,7 @@ let JimoLink: FC<IProps> = React.memo((props) => {
   let { text, className, onClick, ...restProps } = props;
 
   return (
-    <a tabIndex={0} className={cx(styleLink, props.className)} onClick={props.onClick} {...restProps}>
+    <a tabIndex={0} className={cx(styleLink, props.className)} {...restProps}>
       {props.text || props.children || "-"}
     </a>
   );
